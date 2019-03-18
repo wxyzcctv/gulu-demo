d7e318e0ea3109a8251d4b13ebfc33dd1f5f9739
wxyzcctv <2846629226@qq.com>
2019-03-18 19:28:45 +0800

    添加icon和iconposition属性

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12396,7 +12396,16 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+var _default = {
+  props: ["icon", "iconPosition"]
+};
 exports.default = _default;
         var $c8a2ba = exports.default || module.exports;
       
@@ -12407,10 +12416,26 @@ exports.default = _default;
         /* template */
         Object.assign($c8a2ba, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12487,7 +12512,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1694" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8190" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
