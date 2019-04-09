4b68b042a8895382df0eedbc39c3b7ad98b9bc6a
wxyzcctv <2846629226@qq.com>
2019-04-09 09:06:17 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 <script>
+  let validator = (value)=>{
+    let valid = true;
+    let keys = Object.keys(value);
+    keys.forEach(key => {
+      if (!["offset", "span"].includes(key)) {
+        valid = false;
+      }
+    });
+    return valid;
+  }
 export default {
   name: "GuluCol",
   props: {
@@ -12,25 +22,59 @@ export default {
     },
     offset: {
       type: [Number, String]
-    }
-  },
-  data() {
-    return {
-      gutter: 0
-    };
-  },
-  computed: {
-    colClass() {
-      let { span, offset } = this;
-      return [span && `col-${span}`, offset && `offset-${offset}`];
     },
-    colStyle() {
+    phone: {
+      type: Object,
+      validator,
+    },
+    // 输入的phon数组对象的时候，当其key不是offset或者span的时候就会返回false
+    // 也就没有办法进行下面的操作，以上内容是一个算法，这个算法进行了两次循环，
+    // 第一个循环是将得到的数组对象所有的key取出，
+    // 第二个循环是判断所有的key中是否包含了指定的内容,如果不是指定的内容,
+    // 那么就返回一个错误
+    ipad: {
+      type: Object,
+      validator,
+    },
+    narrowPc: {
+      type: Object,
+      validator,
+    },
+    pc: {
+      type: Object,
+      validator,
+    },
+    widePc: {
+      type: Object,
+      validator,
+    },
+  },
+    data() {
       return {
-        paddingLeft: this.gutter / 2 + "px",
-        paddingRight: this.gutter / 2 + "px"
+        gutter: 0
       };
+    },
+    computed: {
+      colClass() {
+        let { span, offset, phone, ipad, narrowPc, pc, widePc} = this;
+        let phoneClass = [];
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`,
+          ...[phone && `col-phone-${phone.span}`],
+          ...[ipad && `col-ipad-${ipad.span}`],
+          ...[narrowPc && `col-narrow-pc-${narrowPc.span}`],
+          ...[pc && `col-pc-${pc.span}`],
+          ...[widePc && `col-wide-pc-${widePc.span}`],
+        ];
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + "px",
+          paddingRight: this.gutter / 2 + "px"
+        };
+      }
     }
-  }
 };
 </script>
 
@@ -46,6 +90,76 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: ($n / 24) * 100%;
+    }
+  }
+  @media (max-width: 576px) {
+    $class-prefix: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
     }
   }
 }
