4d40aa226dabca76b904dd9f18e8b47849ac2c85
wxyzcctv <2846629226@qq.com>
2019-04-10 16:50:00 +0800

    美化了设计布局的样式

--- src/sider.vue
+++ src/sider.vue
@@ -1,16 +1,36 @@
 <template>
-    <div class="sider">
-        <slot></slot>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <button @click="visible=false">close</button>
+      <slot></slot>
     </div>
+  </transition>
 </template>
+
 <script>
 export default {
-    name: 'GuLuLayout'
-}
+  name: "GuLuLayout",
+  data() {
+    return {
+      visible: true
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .sider{
-        
-    }
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.slide-enter-active,.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-enter,.slide-leave-to {
+  margin-left: -200px;
+}
 </style>
