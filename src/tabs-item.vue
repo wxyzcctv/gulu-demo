75487f83e46292409fe6f130f5af8f29a7b76a3a
wxyzcctv <2846629226@qq.com>
2019-04-19 09:22:41 +0800

    添加部分tabs的测试代码

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="onClick" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes" :data-name="name">
         <slot></slot>
     </div>
 </template>
@@ -31,19 +31,21 @@ export default {
         }
     },
     created(){
-        console.log()
-        this.eventBus.$on('update:selected',(name)=>{
-            if(name === this.name){
-                this.active = true
-            }else{
-                this.active = false
-            }
-        })
+        if(this.eventBus){
+            this.eventBus.$on('update:selected',(name)=>{
+                if(name === this.name){
+                    this.active = true
+                }else{
+                    this.active = false
+                }
+            })
+        }
     },
     methods:{
         onClick(){
             if(this.disabled){return}
-            this.eventBus.$emit('update:selected',this.name,this)
+            this.eventBus && this.eventBus.$emit('update:selected',this.name,this)
+            this.$emit('click',this)
         }
     }
 }
@@ -64,6 +66,7 @@ $disabled-text-color:grey;
     }
     &.disabled{
         color: $disabled-text-color;
+        cursor: not-allowed;
     }
 }
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -32,6 +32,10 @@ export default {
         }
     },
     mounted(){
+        if(this.$children.length === 0){
+            console && console.warn &&
+            console.warn('tabs的子组件应该是tabs-head和tabs-body，而你没有写子组件')
+        }
         this.$children.forEach((vm)=>{
             if(vm.$options.name === 'GuLuTabsHead'){
                 vm.$children.forEach((childvm)=>{
