32905d7b4ad57f765af17061efea498630509d7f
wxyzcctv <2846629226@qq.com>
2019-04-24 20:49:18 +0800

    添加button组件的说明

--- docs/components/Button.md
+++ docs/components/Button.md
@@ -4,3 +4,5 @@ title:按钮
 # 按钮
 
 使用方法
+
+<button-demos></button-demos>
--- src/button.vue
+++ src/button.vue
@@ -3,7 +3,7 @@
   @click="$emit('click')">
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon class='loading icon' v-if="loading" name='loading'></g-icon>
-    <div class="content">
+    <div class="g-button-content">
       <slot></slot>
     </div>
   </button>
@@ -35,47 +35,33 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.g-button {
-  @keyframes spain {
-    0%{transform: rotate(0deg)}
-    100%{transform: rotate(360deg)}
-  }
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  vertical-align: middle;
+$font-size:14px;
+$button-height:32px;
+$button-bg:white;
+$button-active-bg:#eee;
+$border-radius: 4px;
+$color:#333;
+$border-color: #999;
+$border-color-hover:#666;
+@keyframes spain {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
+}
+.g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
+  border-radius: $border-radius; border: 1px solid $border-color;
+  background: $button-bg; display: inline-flex; justify-content: center;
+  align-items: center; vertical-align: middle;
   // 解决inline不对齐的情况
-  &:hover {
-    border-color: var(--border-color-hover);
-  }
-  &:active {
-    background: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
-  }
-  >.icon{
-    order: 1;margin-right: .1em;
-  }
-  >.content{
-    order: 2; 
-  }
+  &:hover { border-color: $border-color-hover; }
+  &:active { background: $button-active-bg; }
+  &:focus { outline: none; }
+  >.icon{ order: 1;margin-right: .1em; }
+  >.g-button-content{ order: 2; }
+
   &.icon-right {
-    > .icon {
-      order: 2;margin-left: .1em;margin-right: 0;
-    }
-    > .content {
-      order: 1;
-    }
+    > .icon { order: 2;margin-left: .1em;margin-right: 0; }
+    > .g-button-content { order: 1; }
   }
-  .loading{
-    animation: spain 1s infinite linear;
-  }
+  .loading{ animation: spain 1s infinite linear; }
 }
 </style>
