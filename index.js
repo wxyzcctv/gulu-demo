a0053b440b8419a5d2966889da27f8ac33ea3a50
wxyzcctv <2846629226@qq.com>
2019-05-03 21:59:09 +0800

    发布新的版本

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
+import Input from './src/input'
+import Row from './src/row'
+import Col from './src/col'
+import Layout from './src/layout'
+import Content from './src/content'
+import Header from './src/header'
+import Sider from './src/sider'
+import Footer from './src/footer'
+import Toast from './src/toast'
+import plugin from './src/plugin'
+import Tabs from './src/tabs'
+import TabsHead from './src/tabs-head'
+import TabsBody from './src/tabs-body'
+import TabsItem from './src/tabs-item'
+import TabsPane from './src/tabs-pane'
+import Popover from './src/popover'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapseItem'
 
-export{Button, ButtonGroup, Icon}
+export{Button, ButtonGroup, Icon, Input,
+    Row,Col,Layout,Content,Header,Sider,Footer,Toast,plugin, Tabs, TabsHead, 
+    TabsItem, TabsBody, TabsPane, Popover, Collapse, CollapseItem
+}
